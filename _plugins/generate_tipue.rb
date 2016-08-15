dec51a250fdeb922dce69dcf96010b3424f161fb
Kirstin Hall <jgroto@gmail.com>
2016-08-15 21:43:28 +0100

    added 'unedited' form of search content

--- _plugins/generate_tipue.rb
+++ _plugins/generate_tipue.rb
@@ -60,7 +60,7 @@ module Jekyll
 		def generate(site)
 			
 			pages=Array.new
-			target = File.open('js/tipuesearch_content.js', 'w')
+			target = File.open('assets/src/tipuesearch/tipuesearch_content_unedited.js', 'w')
 			target.truncate(target.size)
 			target.puts('var tipuesearch = {"pages": [')
 			
