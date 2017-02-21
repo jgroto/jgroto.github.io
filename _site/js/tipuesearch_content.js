7c6b796f45eac3379d0771d70536a22d521a45f5
John Grotelueschen <jgroto@github.com>
2017-02-21 00:01:08 +0000

    removed _site from gitignore

---
+++ _site/js/tipuesearch_content.js
@@ -0,0 +1,11 @@
+var tipuesearch = {"pages": [
+{"title":"404 - file not found","tags":" ","url":"/404.html","text":"Ex-Pat Pup provides resources and information to move your pet from the US to the UK on your own with ease."},
+{"title":"About","tags":" ","url":"/about/","text":"All the information you need to move your pet from the US to the UK"},
+{"title":"Contact","tags":" ","url":"/contact-confirm/","text":"How to contact us"},
+{"title":"Contact","tags":" ","url":"/contact/","text":"How to contact us"},
+{"title":null,"tags":" ","url":"/feed.xml","text":""},
+{"title":"Moving your pet from the US to the UK","tags":" ","url":"/","text":"Ex-Pat Pup provides resources and information to move your pet from the US to the UK on your own with ease"},
+{"title":null,"tags":" ","url":"/css/main.css","text":""},
+{"title":"Privacy","tags":" ","url":"/privacy/","text":"ExPatPup's privacy policy"},
+{"title":"Search","tags":" ","url":"/search/","text":""}
+]};
